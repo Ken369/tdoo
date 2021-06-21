373d00471fde5dafb476fbfa159e5751b9acddbf
Ken Power <kpowerperth@gmail.com>
2021-06-21 23:24:53 +0800

    First push, already started offline

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import Todo from "./components/Todo";
+
+function App(props) {
+  return (
+    <div className="todoapp stack-large">
+      <h1>To Do</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            Whats your next Task?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+  role="list"
+  className="todo-list stack-large stack-exception"
+  aria-labelledby="list-heading"
+>
+  <Todo name="Listen to Dire Straits"/>
+  <Todo name="Figure out how to make a React Todo"/>
+  <Todo name="Design"/>
+</ul>
+    </div>
+  );
+}
+
+export default App;
